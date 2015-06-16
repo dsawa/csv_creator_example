f00a8dbed87fb9ac824e4c1a872b3144ba0a80ea
Dorian Sawa <dorian.sawa@gmail.com>
2015-06-16 20:03:12 +0200

    Added ReactJS, 5 zombies for a start

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+zombies_data = [
+  { name: 'Dorian', rotting: false, age: 25, turn_date: DateTime.new(2015, 4, 10) },
+  { name: 'John', rotting: false, age: 15, turn_date: DateTime.new(2015, 3, 5) },
+  { name: 'Jane', rotting: false, age: 18, turn_date: DateTime.new(2015, 2, 15) },
+  { name: 'Chris', rotting: true, age: 45, turn_date: DateTime.new(2015, 4, 22) },
+  { name: 'Rick', rotting: true, age: 64, turn_date: DateTime.new(2015, 5, 12) }
+]
+
+Zombie.create(zombies_data)
+puts "#{zombies_data.size} zombies created!"
